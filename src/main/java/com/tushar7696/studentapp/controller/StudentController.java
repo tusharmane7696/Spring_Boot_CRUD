8b82df6d1256f6b7127d35e1986e5d3a7dc0b0c3
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-07-29 22:42:56 +0530

    feature/swagger-ui and unit test

--- src/main/java/com/tushar7696/studentapp/controller/StudentController.java
+++ src/main/java/com/tushar7696/studentapp/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.tushar7696.studentapp.controller;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,26 +17,31 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @Operation(summary = "Create a student")
     @PostMapping
     public Student createStudent(@RequestBody Student student) {
         return studentService.saveStudent(student);
     }
 
+    @Operation(summary = "Get all students.")
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
+    @Operation(summary = "Get student by ID")
     @GetMapping("/{id}")
     public Student getStudentById(@PathVariable Long id) {
         return studentService.getStudentById(id);
     }
 
+    @Operation(summary = "Update student by ID")
     @PutMapping("/{id}")
     public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
         return studentService.updateStudent(id, student);
     }
 
+    @Operation(summary = "Delete student by ID")
     @DeleteMapping("/{id}")
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
